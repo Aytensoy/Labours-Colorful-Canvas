5ce7c0f0717fb3f9e71a123e6c5a7e9b92126272
Aytensoy <157885112+Aytensoy@users.noreply.github.com>
2025-09-04 21:13:22 +0300

    Some bugs fixed.

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
 // =================================================================
-// SERVICE WORKER - NİHAİ, GÜVENLİ VE TAM KAPSAMLI SÜRÜM (v26)
+// SERVICE WORKER - NİHAİ, GÜVENLİ VE TAM KAPSAMLI SÜRÜM (v27)
 // =================================================================
 
-const CACHE_NAME = 'magical-coloring-v26-objecturl-fallback)';
+const CACHE_NAME = 'magical-coloring-v27-objecturl-fallback)';
 
 const FILES_TO_CACHE = [
     // Çekirdek Dosyalar
