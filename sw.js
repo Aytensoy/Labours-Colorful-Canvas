b70f0f0d2b3a200bfac08e411386df708034ef78
Aytensoy <157885112+Aytensoy@users.noreply.github.com>
2026-03-13 12:32:57 +0300

    Some bugs fixed.

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
 // =================================================================
-// SERVICE WORKER - NİHAİ, GÜVENLİ VE TAM KAPSAMLI SÜRÜM (A49)
+// SERVICE WORKER - NİHAİ, GÜVENLİ VE TAM KAPSAMLI SÜRÜM (A50)
 // =================================================================
 
-const CACHE_NAME = 'magical-coloring-A49';
+const CACHE_NAME = 'magical-coloring-A50';
 
 const FILES_TO_CACHE = [
     // Çekirdek Dosyalar
